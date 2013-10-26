db3b827c8c59f9e5e40134abb159b7cc7a3e05c6
edd44 <kaptur.michal@gmail.com>
2013-10-26 15:26:00 +0200

    Base App
    Detecting joy and polling 3 axis values on timer events

---
+++ HID_DirectInput_joy/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace HID_DirectInput_joy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBoxLog = new System.Windows.Forms.ListBox();
+            this.buttonDetectJoy = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.trackBarX = new System.Windows.Forms.TrackBar();
+            this.trackBarY = new System.Windows.Forms.TrackBar();
+            this.trackBarZ = new System.Windows.Forms.TrackBar();
+            this.timerJoyPolling = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarX)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarY)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarZ)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBoxLog
+            // 
+            this.listBoxLog.FormattingEnabled = true;
+            this.listBoxLog.Location = new System.Drawing.Point(19, 209);
+            this.listBoxLog.Name = "listBoxLog";
+            this.listBoxLog.Size = new System.Drawing.Size(566, 43);
+            this.listBoxLog.TabIndex = 0;
+            // 
+            // buttonDetectJoy
+            // 
+            this.buttonDetectJoy.Location = new System.Drawing.Point(18, 13);
+            this.buttonDetectJoy.Name = "buttonDetectJoy";
+            this.buttonDetectJoy.Size = new System.Drawing.Size(88, 24);
+            this.buttonDetectJoy.TabIndex = 1;
+            this.buttonDetectJoy.Text = "Wyszukaj joya";
+            this.buttonDetectJoy.UseVisualStyleBackColor = true;
+            this.buttonDetectJoy.Click += new System.EventHandler(this.buttonDetectJoy_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(150, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(150, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Y";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(150, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(14, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Z";
+            // 
+            // trackBarX
+            // 
+            this.trackBarX.Location = new System.Drawing.Point(170, 17);
+            this.trackBarX.Maximum = 65536;
+            this.trackBarX.Name = "trackBarX";
+            this.trackBarX.Size = new System.Drawing.Size(104, 45);
+            this.trackBarX.TabIndex = 11;
+            // 
+            // trackBarY
+            // 
+            this.trackBarY.Location = new System.Drawing.Point(170, 55);
+            this.trackBarY.Maximum = 65536;
+            this.trackBarY.Name = "trackBarY";
+            this.trackBarY.Size = new System.Drawing.Size(104, 45);
+            this.trackBarY.TabIndex = 12;
+            // 
+            // trackBarZ
+            // 
+            this.trackBarZ.Location = new System.Drawing.Point(170, 92);
+            this.trackBarZ.Maximum = 65536;
+            this.trackBarZ.Name = "trackBarZ";
+            this.trackBarZ.Size = new System.Drawing.Size(104, 45);
+            this.trackBarZ.TabIndex = 13;
+            // 
+            // timerJoyPolling
+            // 
+            this.timerJoyPolling.Tick += new System.EventHandler(this.timerJoyPolling_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(611, 262);
+            this.Controls.Add(this.trackBarZ);
+            this.Controls.Add(this.trackBarY);
+            this.Controls.Add(this.trackBarX);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonDetectJoy);
+            this.Controls.Add(this.listBoxLog);
+            this.Name = "Form1";
+            this.Text = "v";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarX)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarY)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarZ)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxLog;
+        private System.Windows.Forms.Button buttonDetectJoy;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TrackBar trackBarX;
+        private System.Windows.Forms.TrackBar trackBarY;
+        private System.Windows.Forms.TrackBar trackBarZ;
+        private System.Windows.Forms.Timer timerJoyPolling;
+    }
+}
+
---
+++ HID_DirectInput_joy/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SharpDX.DirectInput;
+
+namespace HID_DirectInput_joy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Initialize DirectInput
+        DirectInput directInput = new DirectInput();
+
+        // Find a Joystick Guid
+        Guid joystickGuid;
+
+        Joystick joystick;
+
+        private void buttonDetectJoy_Click(object sender, EventArgs e)
+        {
+            foreach (var deviceInstance in directInput.GetDevices(DeviceType.Gamepad,
+                DeviceEnumerationFlags.AllDevices))
+                joystickGuid = deviceInstance.InstanceGuid;
+
+            // If Gamepad not found, look for a Joystick
+            if (joystickGuid == Guid.Empty)
+                foreach (var deviceInstance in directInput.GetDevices(DeviceType.Joystick,
+                        DeviceEnumerationFlags.AllDevices))
+                    joystickGuid = deviceInstance.InstanceGuid;
+
+            if (joystickGuid == Guid.Empty)
+            {
+                listBoxLog.Items.Insert(0, "nie znaleziono joya");
+            }
+
+            else
+            {
+                joystick = new Joystick(directInput, joystickGuid);
+                listBoxLog.Items.Insert(0, "znalazłem joya, GUID: " + joystickGuid);
+
+                //forcefeedback check
+                var allEffects = joystick.GetEffects();
+                if (allEffects.Count == 0)
+                    listBoxLog.Items.Insert(0, "brak ForceFeedbacka");
+                else
+                {
+                    foreach (var effectInfo in allEffects)
+                        listBoxLog.Items.Insert(0, "Effect available " + effectInfo.Name);
+                }
+
+                //Set BufferSize in order to use buffered data.
+                joystick.Properties.BufferSize = 128;
+
+
+                // Acquire the joystick
+                joystick.Acquire();
+
+                timerJoyPolling.Enabled = true;
+            }
+        }
+
+            
+
+            private void timerJoyPolling_Tick(object sender, EventArgs e)
+            {
+                //listBoxLog.Items.Insert(0, "tick");
+                joystick.Poll();
+                var datas = joystick.GetBufferedData();
+                foreach (var state in datas)
+                {
+                    //listBoxLog.Items.Insert(0, state);
+                    switch (state.Offset)
+                    {
+                        case JoystickOffset.X:
+                            //textBoxXvalue.Text = state.Value.ToString();
+                            //  progressBarX.Value =  state.Value;
+                            trackBarX.Value = state.Value;
+                            break;
+
+                        case JoystickOffset.Y:
+                            // textBoxYvalue.Text = state.Value.ToString();
+                            //  progressBarY.Value =  state.Value;
+                            trackBarY.Value = state.Value;
+                            break;
+
+                        case JoystickOffset.Z:
+                            //textBoxZvalue.Text = state.Value.ToString();
+                            // progressBarZ.Value =  state.Value;
+                            trackBarZ.Value = state.Value;
+                            break;
+
+                        default:
+                            break;
+                    }
+                }
+            }
+                
+
+        }
+    }
+
